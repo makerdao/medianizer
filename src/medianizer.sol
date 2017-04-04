537948d3fb24a2c1fecad43f749ae80dbb58ee86
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-04-04 15:53:51 -0300

    Set Unset functions

--- src/medianizer.sol
+++ src/medianizer.sol
@@ -2,15 +2,43 @@ pragma solidity ^0.4.8;
 
 import 'ds-cache/cache.sol';
 
-contract Medianizer is DSCache {
+contract MedianizerEvents {
+    event LogSet(bytes12 pos, address wat);
+}
+
+contract Medianizer is DSCache, MedianizerEvents {
     mapping (bytes12 => DSValue) public values;
+    mapping (address => bytes12) public indexes;
+
     bytes12 public next = 0x1;
     
-    function set(DSValue wat) auth {
+    function set(address wat) auth {
         bytes12 nextId = bytes12(uint96(next) + 1);
         assert(nextId != 0x0);
-        values[next] = wat;
+        set(next, wat);
         next = nextId;
+    }
+
+    function set(bytes12 pos, address wat) auth {
+        if (indexes[wat] != 0 && pos != 0x0) throw;
+
+        if (wat == 0) {
+            indexes[values[pos]] = 0;
+        } else {
+            indexes[wat] = pos;
+        }
+
+        values[pos] = DSValue(wat);
+
+        LogSet(pos, wat);
+    }
+
+    function unset(bytes12 pos) {
+        set(pos, 0);
+    }
+
+    function unset(address wat) {
+        set(indexes[wat], 0);
     }
 
     function poke() auth {
@@ -32,20 +60,22 @@ contract Medianizer is DSCache {
         bytes32[] memory wuts = new bytes32[](uint96(next) - 1);
         uint96 ctr = 0;
         for (uint96 i = 1; i < uint96(next); i++) {
-            bytes32 wut = values[bytes12(i)].read();
-            if (ctr == 0 || wut >= wuts[ctr - 1]) {
-                wuts[ctr] = wut;
-            } else {
-                uint96 j = 0;
-                while (wut >= wuts[j]) {
-                    j++;
+            if (address(values[bytes12(i)]) != 0) {
+                bytes32 wut = values[bytes12(i)].read();
+                if (ctr == 0 || wut >= wuts[ctr - 1]) {
+                    wuts[ctr] = wut;
+                } else {
+                    uint96 j = 0;
+                    while (wut >= wuts[j]) {
+                        j++;
+                    }
+                    for (uint96 k = ctr; k > j; k--) {
+                        wuts[k] = wuts[k - 1];
+                    }
+                    wuts[j] = wut;
                 }
-                for (uint96 k = ctr; k > j; k--) {
-                    wuts[k] = wuts[k - 1];
-                }
-                wuts[j] = wut;
+                ctr++;
             }
-            ctr++;
         }
 
         if (ctr % 2 == 0) {
