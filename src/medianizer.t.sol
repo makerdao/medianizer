efc685f9cf4cf6b88668c5b019dacaa5743973e0
Mariano Conti <nanexcool@gmail.com>
2018-09-19 12:11:45 -0300

    Update to solc 0.4.24

--- src/medianizer.t.sol
+++ src/medianizer.t.sol
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 import "ds-test/test.sol";
 import "ds-value/value.sol";
